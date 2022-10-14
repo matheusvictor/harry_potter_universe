2a042420a1295ba054c9172f74d3a5c421c6a777
Matheus Victor <matheusvictor.salles@gmail.com>
2022-10-14 14:46:28 -0300

    fix: Add new constants to endpoints

--- constants/Constants.py
+++ constants/Constants.py
@@ -1,3 +1,7 @@
 base_url = 'https://hp-api.onrender.com'
-characters = '/api/characters'
-spells = '/api/spells'
+
+characters = '/api/characters'  # all characters
+students = f'{characters}/students'  # characters who are Hogwarts students during the book series
+staff = f'{characters}/staff'  # characters who are Hogwarts staff during the book series
+characters_by_house = '/api/charactershouse/:house'  # characters in a certain house, e.g. /gryffindor
+spells = '/api/spells'  # all spells
