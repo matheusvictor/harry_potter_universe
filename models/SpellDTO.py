76266a7ee5b1f361d0579b1fac6b5a6abc84e769
Matheus Victor <matheusvictor.salles@gmail.com>
2022-10-14 14:45:32 -0300

    feature: Add DTO to Spell

---
+++ models/SpellDTO.py
@@ -0,0 +1,12 @@
+class SpellDTO:
+    def __init__(self, name, description):
+        self.__name = name
+        self.__description = description
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def description(self):
+        return self.__description
